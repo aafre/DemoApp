27d3eea48b803bbd35fa62e57e5f17c5c4ce6d96
Amit Afre <amit.afre@no-domain.com>
2016-03-24 22:13:59 +0530

    Login page view complete

--- WebContent/app/core/route.js
+++ WebContent/app/core/route.js
@@ -10,12 +10,27 @@
 		      templateUrl   : "app/core/views/home.html",
 		      controller    : "HomeCtrl"
 		  	})
+		  	.state("register", {
+		      url          : "/register",
+		      templateUrl   : "app/modules/register/views/register.html",
+		      controller    : "RegisterController"
+		  	})
+		  	.state("login", {
+		      url          : "/login",
+		      templateUrl   : "app/modules/login/views/login.html",
+		      controller    : "LoginController"
+		  	})
+		  	.state("dashboard", {
+			      url          : "/dashboard",
+			      templateUrl   : "app/modules/dashboard/views/dashboard.html",
+			      controller    : "DashboardController"
+			  	})
 		   .state('modules', {
 		      url: '/modules',
 		      "abstract" : true,
 		      templateUrl:"app/core/views/module.html"
 		    });
-		   
+		    
 		// if none of the above states are matched, use this as the fall-back
 		  
 		  $urlRouterProvider.otherwise('home');
